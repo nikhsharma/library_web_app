b06bd5dc346780fb9bfc180be06aaa75d647b0f1
Ruaridh <ruaridhdunbar@gmail.com>
2018-05-23 15:28:50 +0100

    adds Library class, properties, getters and setters. Creates new instance of library  in dbrunner.

--- src/main/java/DBRunner.java
+++ src/main/java/DBRunner.java
@@ -1,18 +1,22 @@
 import db.DBHelper;
 import models.Book;
 import models.Borrower;
+import models.Library;
 
 public class DBRunner {
 
     public static void main(String[] args) {
 
-        Book book1 = new Book("Harry Potter and Prisoner of Azkaban ", "J.K Rowling");
+        Library library1 = new Library("The Grand Library");
+        DBHelper.save(library1);
+
+        Book book1 = new Book("Harry Potter and Prisoner of Azkaban ", "J.K Rowling", library1);
         DBHelper.save(book1);
-        Book book2 = new Book("1984", "George Orwell");
+        Book book2 = new Book("1984", "George Orwell", library1);
         DBHelper.save(book2);
-        Book book3 = new Book ("Lord of the Flies", "William Golding");
+        Book book3 = new Book ("Lord of the Flies", "William Golding", library1);
         DBHelper.save(book3);
-        Book book4 = new Book("Animal Farm", "George Orwell");
+        Book book4 = new Book("Animal Farm", "George Orwell", library1);
         DBHelper.save(book4);
 
         Borrower borrower1 = new Borrower("Nikhil Sharma");
